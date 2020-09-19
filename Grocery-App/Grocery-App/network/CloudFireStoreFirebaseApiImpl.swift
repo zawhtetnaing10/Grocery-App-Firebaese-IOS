6f1d5be4abd20891e9579055f2a786bb7125d33c
zawhtetnaing10 <zawgyi.gog@gmail.com>
2020-09-19 12:34:06 +0630

    Firestore installed

---
+++ Grocery-App/Grocery-App/network/CloudFireStoreFirebaseApiImpl.swift
@@ -0,0 +1,23 @@
+//
+//  CloudFireStoreFirebaseApiImpl.swift
+//  Grocery-App
+//
+//  Created by Zaw Htet Naing on 19/09/2020.
+//  Copyright © 2020 Zaw Htet Naing. All rights reserved.
+//
+
+import Foundation
+
+class CloudFireStoreFirebaseApiImpl : FirebaseApi{
+    func getAllGroceries(success: @escaping ([GroceryVO]) -> Void, failure: @escaping (String) -> Void) {
+        
+    }
+    
+    func addGrocery(grocery: GroceryVO) {
+        
+    }
+    
+    func deleteGrocery(grocery: GroceryVO) {
+    
+    } 
+}
